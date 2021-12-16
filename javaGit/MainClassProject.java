26623d3b7b7ec4590804463d64af0247d5b5cedd
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2021-12-16 14:35:00 +0100

    Premier commit sans l'autre

---
+++ javaGit/MainClassProject.java
@@ -0,0 +1,14 @@
+package javaGit;
+
+public class MainClassProject {
+	
+	public static void main(String[] args) {
+		{
+		// TODO Auto-generated method stub
+		System.out.println("Un");
+		System.out.println("Deux");
+		System.out.println("Trois");
+		}
+		System.out.println("Exit");
+	}
+}
