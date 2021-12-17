533a894b4a669d86ff5de21c29d80f71b4dd7580
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2021-12-17 15:41:08 +0100

    tp4 pré exo1

--- suite.java
+++ suite.java
@@ -7,10 +7,67 @@ import java.util.Scanner;
 public class suite {
 
 	public static void main(String[] args) {
-		
-		
+		for (int i = 0; i < 12; i++) {
+		      switch (i) {
+		        case 9:
+		          System.out.println("Neuf");
+		          break;
+		        case 8:
+		          System.out.print("Huit");
+		          System.out.println("");
+		          break;
+		        case 7:
+		          {
+		            System.out.print("Sept");
+		            System.out.println("");
+		          }
+		          break;
+		        case 6:
+		          System.out.print("Six ");
+		        case 5:
+		          System.out.println("Cinq");
+		          break;
+		        case 4:
+		        case 3:
+		          System.out.print("Quatre Trois ");
+		        case 2:
+		          System.out.println("Deux");
+		          break;
+		        case 1:
+		          System.out.println("Un");
+		          break;
+		        default:
+		          System.out.println("Zero");
+		          break;
+		      }
+		    }
+		 System.out.println("Fin");
 	}
 }
+/*int x = 1; 
+		double a = 0;
+		for( x = 1; x <= 10; x++){
+		     a = x - 7;
+		     if (a == 0){
+		         System.out.println("Division par 0");
+		         break; 
+		       }
+		     System.out.println(1/a);
+		    }*/	
+
+/*int x = 1;
+	    float a;
+	    while (x <= 10) {
+	      x++;
+	      System.out.print("x = " + x);
+	      if (x == 7) {
+	        System.out.println("\tDivision par zero!");
+	        continue;
+	      }
+	      a = (float) 1 / (x - 7);
+	      System.out.println(" \ta = " + a);
+	    }*/
+
 		/*Scanner sc = new Scanner(System.in);
 		int valeur;
 		String chaine_lue;
